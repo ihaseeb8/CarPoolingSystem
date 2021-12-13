79b72b1c23014897da10f5b39f63d77072602394
Haseebullah <ihaseebullah.8@gmail.com>
2021-12-14 00:53:01 +0500

    New

--- src/main/java/com/example/aproj/LoginViewController.java
+++ src/main/java/com/example/aproj/LoginViewController.java
@@ -1,11 +1,17 @@
 package com.example.aproj;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginViewController {
 
@@ -36,4 +42,15 @@ public class LoginViewController {
 
     }
 
+    @FXML
+    void LoginButtonPressed(MouseEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("profiles-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        //Node node = (Node) event.getSource();
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
--- src/main/java/com/example/aproj/SignUpViewController.java
+++ src/main/java/com/example/aproj/SignUpViewController.java
@@ -1,4 +1,49 @@
 package com.example.aproj;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Paint;
+
 public class SignUpViewController {
+
+    @FXML
+    private TextField CnicField;
+
+    @FXML
+    private PasswordField ConfirmPassField;
+
+    @FXML
+    private TextField EmailField;
+
+    @FXML
+    private TextField NameField;
+
+    @FXML
+    private PasswordField PassField;
+
+    @FXML
+    private TextField PhoneNoField;
+
+    @FXML
+    private Button SignUpButton;
+
+
+    @FXML
+    void onSignUpButtonMouseMoved(MouseEvent event) {
+        SignUpButton.setStyle("-fx-background-color: #F44336");
+        SignUpButton.setTextFill(Paint.valueOf("#FFFFFF"));
+    }
+
+    @FXML
+    void onSignUpButtonMouseRemoved(MouseEvent event) {
+        SignUpButton.setStyle("-fx-background-color: #ff725e");
+        SignUpButton.setTextFill(Paint.valueOf("#000000"));
+    }
+    @FXML
+    void SignUpButtonClicked(MouseEvent event) {
+
+    }
 }
